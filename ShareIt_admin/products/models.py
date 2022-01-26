720719c83f78a4a66bec79a64baa00672da76a33
Ibrahim Shittu <ibshittu01@gmail.com>
2022-01-26 19:29:09 +0100

    models

--- ShareIt_admin/products/models.py
+++ ShareIt_admin/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    title = models.CharField(max_length=256)
+    image = models.FileField(upload_to='images')
+    likes = models.PositiveIntegerField(default=0)
+
+
+class User(models.Model):
+    pass
